f2e2dee18debe1e26697a3bc3ef754bb5e6e0379
christine <credhead89@gmail.com>
2020-03-11 17:09:47 -0400

    Update student information in show page

--- src/views/StudentsShow.vue
+++ src/views/StudentsShow.vue
@@ -11,9 +11,34 @@
     <h3>Resume: {{ student.online_resume_url }}</h3>
     <h3>Github: {{ student.github_url }}</h3>
     <h3>Photo: {{ student.photo }}</h3>
-    <!-- <div v-for="student in student.education">
-      <p>{{ student.degree }}</p>
-    </div> -->
+    <div v-for="education in student.educations">
+      <h1>Education</h1>
+      <p>Start Date: {{ education.start_date }}</p>
+      <p>End Date: {{ education.end_date }}</p>
+      <p>Degree: {{ education.degree }}</p>
+      <p>University: {{ education.university_name }}</p>
+      <p>Details: {{ education.details }}</p>
+      <router-link :to="`/educations/${education.id}/edit`">Edit</router-link>
+    </div>
+    <div v-for="experience in student.experiences">
+      <h1>Experience</h1>
+      <p>Start Date: {{ experience.start_date }}</p>
+      <p>End Date: {{ experience.end_date }}</p>
+      <p>Job Title: {{ experience.job_title }}</p>
+      <p>Company: {{ experience.company_name }}</p>
+      <p>Details: {{ experience.details }}</p>
+    </div>
+    <div v-for="capstone in student.capstones">
+      <h1>Capstone</h1>
+      <p>Name: {{ capstone.name }}</p>
+      <p>description: {{ capstone.description }}</p>
+      <p>Url: {{ capstone.url }}</p>
+      <p>Screenshot: {{ capstone.screenshot }}</p>
+    </div>
+    <div v-for="skill in student.skills">
+      <h1>Skill</h1>
+      <p>Name: {{ skill.name }}</p>
+    </div>
   </div>
 </template>
 
