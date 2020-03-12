8bb7a7c622c1828980ab2bdb349138e5068aee6f
christine <credhead89@gmail.com>
2020-03-12 16:13:28 -0400

    add bootstrap to app

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
 import axios from "axios";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-axios.defaults.baseURL = 
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 var jwt = localStorage.getItem("jwt");
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = "Bearer " + jwt;
 }
 
-
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
