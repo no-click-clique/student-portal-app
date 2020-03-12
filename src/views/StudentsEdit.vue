1a8ab51152992b8b711b6a2f7f7888ee8b122fc2
christine <credhead89@gmail.com>
2020-03-12 15:23:33 -0400

    adds current user logic

--- src/views/StudentsEdit.vue
+++ src/views/StudentsEdit.vue
@@ -1,59 +1,55 @@
-
-
 <template>
   <div class="students-edit">
     <div class="container">
       <form v-on:submit.prevent="submit()">
-       <h1>{{ message }}</h1>
+        <h1>{{ message }}</h1>
         <ul>
           <li class="text-danger" v-for="error in errors">{{ error }}</li>
         </ul>
-        
+
         <div class="form-group">
           <label>First Name:</label>
-          <input type="text" class="form-control" v-model="student.first_name">
+          <input type="text" class="form-control" v-model="student.first_name" />
         </div>
         <div class="form-group">
           <label>Last Name:</label>
-          <input type="text" class="form-control" v-model="student.last_name">
+          <input type="text" class="form-control" v-model="student.last_name" />
         </div>
         <div class="form-group">
           <label>Email:</label>
-          <input type="email" class="form-control" v-model="student.email">
+          <input type="email" class="form-control" v-model="student.email" />
         </div>
         <div class="form-group">
           <label>Phone Number:</label>
-          <input type="text" class="form-control" v-model="student.phone_number">
+          <input type="text" class="form-control" v-model="student.phone_number" />
         </div>
         <div class="form-group">
           <label>Short Bio:</label>
-          <input type="text" class="form-control" v-model="student.short_bio">
+          <input type="text" class="form-control" v-model="student.short_bio" />
         </div>
         <div class="form-group">
           <label>LinkedIn:</label>
-          <input type="text" class="form-control" v-model="student.linkedin_url">
+          <input type="text" class="form-control" v-model="student.linkedin_url" />
         </div>
         <div class="form-group">
           <label>Twitter:</label>
-          <input type="text" class="form-control" v-model="student.twitter_handle">
+          <input type="text" class="form-control" v-model="student.twitter_handle" />
         </div>
         <div class="form-group">
           <label>Website:</label>
-          <input type="text" class="form-control" v-model="student.website_url">
+          <input type="text" class="form-control" v-model="student.website_url" />
         </div>
         <div class="form-group">
           <label>Online Resume:</label>
-          <input type="text" class="form-control" v-model="student.online_resume_url">
+          <input type="text" class="form-control" v-model="student.online_resume_url" />
         </div>
-        <input type="submit" class="btn btn-primary" value="Submit">
+        <input type="submit" class="btn btn-primary" value="Submit" />
       </form>
     </div>
-    
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
 
 <script>
 import axios from "axios";
@@ -67,7 +63,7 @@ export default {
     };
   },
   created: function() {
-    axios.get(`/api/educations/${this.$route.params.id}`).then(response => {
+    axios.get(`/api/students/${this.$route.params.id}`).then(response => {
       console.log(response.data);
       this.student = response.data;
     });
@@ -83,7 +79,7 @@ export default {
         linkedin_url: this.student.linkedin_url,
         twitter_handle: this.student.twitter_handle,
         website_url: this.student.website_url,
-        online_resume_url: this.student.online_resume_url,  
+        online_resume_url: this.student.online_resume_url
       };
       axios
         .patch("/api/students/1", params)
@@ -94,7 +90,7 @@ export default {
           console.log(error.response);
           this.errors = error.response.data.errors;
         });
-    },
+    }
   }
 };
 </script>
